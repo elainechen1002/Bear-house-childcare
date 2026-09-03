1b627211706ee6b7f5ccbaf424f6fd6536cb4d62
elainechen1002 <elainechen1002edu@gmail.com>
2026-09-03 10:06:50 -0400

    Add lib/site-config.ts

---
+++ lib/site-config.ts
@@ -0,0 +1,12 @@
+// Contact details live in one place so they can be updated without touching the page layout.
+export const siteConfig = {
+  name: "Bear’s House Child Care Centre",
+  shortName: "Bear’s House",
+  phoneDisplay: "(604) 721-7488",
+  phoneHref: "+16047217488",
+  email: "auroramontessori18@gmail.com",
+  addressLines: ["4609 Rumble Street", "Burnaby, BC V5J 2A8"],
+  mapUrl:
+    "https://www.google.com/maps/search/?api=1&query=4609+Rumble+Street+Burnaby+BC+V5J+2A8",
+  hours: "Monday–Friday · 8:30 a.m.–5:30 p.m.",
+};
